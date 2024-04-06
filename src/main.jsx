b2ce1d7112d5e4057cd63cb87d3348e4d772ac02
Miguel300l <miguelledezma400@gmail.com>
2024-04-05 22:14:33 -0500

    url backend

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import {BrowserRouter} from 'react-router-dom'
 import axios from 'axios' 
 
 
-axios.defaults.baseURL = 'http://localhost:3000';
+axios.defaults.baseURL = 'https://backend-mascotas-three.vercel.app';
 
 
 
