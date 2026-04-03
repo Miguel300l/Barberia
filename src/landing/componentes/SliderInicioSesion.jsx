81d46647a19a6c547ccd857ec01dd5ccc2493a80
Miguel300l <miguelledezma400@gmail.com>
2026-04-02 21:02:37 -0500

    Automatizar en movil

--- src/landing/componentes/SliderInicioSesion.jsx
+++ src/landing/componentes/SliderInicioSesion.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import { Carousel } from "bootstrap";
 
-import barbershop from "../../assets/img/barbershop.webp";
+const barbershop = "/barbershop.webp";
 import barbershop2 from "../../assets/img/barbershop2.jpeg";
 import barbershop3 from "../../assets/img/barbershop3.jpeg";
 import barbershop4 from "../../assets/img/barbershop4.jpeg";
